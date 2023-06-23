8287e9714f463a7ee6e8a9ca237f1982ced0682a
LEESEUNGYEOL <lsy12210@gmail.com>
2023-06-23 11:16:56 +0900

    feat: [#4] OAuth 로그인을 위한 Token Dto

---
+++ src/main/java/com/ssh/dartserver/auth/dto/request/TokenRequestDto.java
@@ -0,0 +1,12 @@
+package com.ssh.dartserver.auth.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRequestDto {
+    private String accessToken;
+}
---
+++ src/main/java/com/ssh/dartserver/auth/dto/response/TokenResponseDto.java
@@ -0,0 +1,13 @@
+package com.ssh.dartserver.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenResponseDto {
+    private String jwtToken;
+    private String providerId;
+}
