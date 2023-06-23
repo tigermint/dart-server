d6651aa4ff32e89654afbfd9a72342343001ac17
LEESEUNGYEOL <lsy12210@gmail.com>
2023-06-23 23:45:22 +0900

    feat: [#7] UniversityDto, mapper 구현

---
+++ src/main/java/com/ssh/dartserver/university/dto/UniversityDto.java
@@ -0,0 +1,10 @@
+package com.ssh.dartserver.university.dto;
+
+import lombok.Data;
+
+@Data
+public class UniversityDto {
+    private Long id;
+    private String name;
+    private String department;
+}
