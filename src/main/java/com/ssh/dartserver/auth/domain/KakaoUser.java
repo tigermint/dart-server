c49cec942bb326fc7470da48258c74591ec8b2a8
LEESEUNGYEOL <lsy12210@gmail.com>
2023-06-23 10:21:13 +0900

    feat: [#4] OAuth를 위한 OAuthUserInfo kakao 구현체

---
+++ src/main/java/com/ssh/dartserver/auth/domain/KakaoUser.java
@@ -0,0 +1,22 @@
+package com.ssh.dartserver.auth.domain;
+
+import java.util.Map;
+
+public class KakaoUser implements OAuthUserInfo {
+
+    private final Map<String, Object> attribute;
+
+    public KakaoUser(Map<String, Object> attribute) {
+        this.attribute = attribute;
+    }
+
+    @Override
+    public String getProviderId() {
+        return String.valueOf(attribute.get("id"));
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+}
