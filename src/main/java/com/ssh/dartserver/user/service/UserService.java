8b4c70730da09165a220f5c170c433b632c4b683
LEESEUNGYEOL <lsy12210@gmail.com>
2023-06-28 15:06:06 +0900

    refactor: [#7] 중복 제거를 위한 create()를 update()로 대체

--- src/main/java/com/ssh/dartserver/user/service/UserService.java
+++ src/main/java/com/ssh/dartserver/user/service/UserService.java
@@ -20,35 +20,21 @@ public class UserService {
     private final UserMapper userMapper;
     private final UniversityRepository universityRepository;
 
-    @Transactional
-    public UserResponseDto create(User user, UserRequestDto userRequestDto) {
-        validateDuplicationUser(user);
-        University university = universityRepository.findById(userRequestDto.getUniversityId())
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 대학교입니다."));
-        user.update(university, userRequestDto);
-        return userMapper.toResponseDto(userRepository.save(user));
-    }
-
-    public UserResponseDto read(User user) {
-        return userMapper.toResponseDto(user);
+    public UserResponseDto read(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+        return userMapper.toResponseDto(user, user.getUniversity());
     }
     @Transactional
     public UserResponseDto update(User user, UserRequestDto userRequestDto) {
         University university = universityRepository.findById(userRequestDto.getUniversityId())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 대학교입니다."));
         user.update(university, userRequestDto);
-        return userMapper.toResponseDto(user);
+        userRepository.save(user);
+        return userMapper.toResponseDto(user, user.getUniversity());
     }
-
+    @Transactional
     public void delete(User user) {
         userRepository.delete(user);
-    }
-
-
-    private void validateDuplicationUser(User user) {
-        userRepository.findById(user.getId())
-                .ifPresent(u -> {
-                    throw new IllegalArgumentException("이미 가입된 회원입니다.");
-                });
     }
 }
