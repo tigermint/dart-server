502dca8eee2361b66d34462318796212797045bc
LEESEUNGYEOL <lsy12210@gmail.com>
2023-06-28 14:53:42 +0900

    feat: [#7] BaseTimeEntity를 위한 JpaAuditingConfig 추가

--- src/main/java/com/ssh/dartserver/common/BaseTimeEntity.java
+++ src/main/java/com/ssh/dartserver/common/BaseTimeEntity.java
@@ -10,8 +10,8 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
-@MappedSuperclass // BaseEntity를 상속한 엔티티들은 아래 필드들을 컬럼으로 인식하게 된다.
-@EntityListeners(AuditingEntityListener.class)  // Auditing(자동으로 값 매핑) 기능 추가
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
     @CreatedDate
