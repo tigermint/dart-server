5cbe05db6a33b3cbcda785ab9e6df526121a4699
LEESEUNGYEOL <lsy12210@gmail.com>
2023-06-28 15:01:19 +0900

    fix: [#7] Mapstruct field 타겟팅 error 해결

--- src/main/java/com/ssh/dartserver/user/infra/mapper/UserMapper.java
+++ src/main/java/com/ssh/dartserver/user/infra/mapper/UserMapper.java
@@ -1,10 +1,18 @@
 package com.ssh.dartserver.user.infra.mapper;
 
+import com.ssh.dartserver.university.domain.University;
 import com.ssh.dartserver.user.domain.User;
 import com.ssh.dartserver.user.dto.UserResponseDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    UserResponseDto toResponseDto(User user);
+
+    @Mapping(target = "userId", source = "user.id")
+    @Mapping(target = "name", source = "user.name")
+    @Mapping(target = "universityName", source = "university.name")
+    @Mapping(target = "universityId", source = "university.id")
+    @Mapping(target = "department", source = "university.department")
+    UserResponseDto toResponseDto(User user, University university);
 }
