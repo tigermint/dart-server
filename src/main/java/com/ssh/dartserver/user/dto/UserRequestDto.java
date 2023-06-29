5ff5c942f8f2a829031c77bc8047fb65bf874ac5
LEESEUNGYEOL <lsy12210@gmail.com>
2023-06-30 01:31:28 +0900

    fix: [#10] userRequestDto validation 휴대폰 번호 정규표현식 수정

--- src/main/java/com/ssh/dartserver/user/dto/UserRequestDto.java
+++ src/main/java/com/ssh/dartserver/user/dto/UserRequestDto.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.Pattern;
 
 @Data
 public class UserRequestDto {
-    @NotNull
+    @NotNull(message = "학교를 선택해주세요")
     private Long universityId;
 
     @NotNull(message = "학번을 입력해주세요")
@@ -18,6 +18,6 @@ public class UserRequestDto {
     private String name;
 
     @NotBlank(message = "휴대폰번호를 입력해주세요")
-    @Pattern(regexp = "^01[016789]-?\\d{3,4}-?\\d{4}$\n", message = "휴대폰번호를 정확히 입력해주세요")
+    @Pattern(regexp = "^01[016789]-?\\d{3,4}-?\\d{4}$", message = "휴대폰번호를 정확히 입력해주세요")
     private String phone;
 }
