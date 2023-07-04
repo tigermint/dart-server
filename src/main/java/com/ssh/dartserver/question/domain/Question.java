8b47c8f43a8c45c6444cd7455bbd3e0812b24c65
LEESEUNGYEOL <lsy12210@gmail.com>
2023-07-04 18:17:20 +0900

    feat: [#14] Question Entity 구현

---
+++ src/main/java/com/ssh/dartserver/question/domain/Question.java
@@ -0,0 +1,30 @@
+package com.ssh.dartserver.question.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Question {
+    @Id @GeneratedValue
+    @Column(name = "question_id")
+    private Long id;
+
+    private String content;
+
+    private String icon;
+
+    @Builder
+    public Question(String content, String icon) {
+        this.content = content;
+        this.icon = icon;
+    }
+}
