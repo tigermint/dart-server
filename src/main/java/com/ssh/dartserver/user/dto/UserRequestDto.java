1808af1bde3d49020c78557f6182b7deaa6aff27
LEESEUNGYEOL <lsy12210@gmail.com>
2023-07-10 15:50:58 +0900

    refactor: [#34] 학번 변수명 수정 - from admissionNum to admissionYear

--- src/main/java/com/ssh/dartserver/user/dto/UserRequestDto.java
+++ src/main/java/com/ssh/dartserver/user/dto/UserRequestDto.java
@@ -1,5 +1,8 @@
 package com.ssh.dartserver.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ssh.dartserver.user.domain.personalinfo.Gender;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -12,7 +15,7 @@ public class UserRequestDto {
     private Long universityId;
 
     @NotNull(message = "학번을 입력해주세요")
-    private int admissionNum;
+    private int admissionYear;
 
     @NotBlank(message = "이름을 입력해주세요")
     private String name;
@@ -20,4 +23,20 @@ public class UserRequestDto {
     @NotBlank(message = "휴대폰번호를 입력해주세요")
     @Pattern(regexp = "^01[016789]-?\\d{3,4}-?\\d{4}$", message = "휴대폰번호를 정확히 입력해주세요")
     private String phone;
+
+    @NotNull(message = "성별을 선택해주세요")
+    private Gender gender;
+
+    @JsonCreator
+    public UserRequestDto(@JsonProperty("universityId") Long universityId,
+                          @JsonProperty("admissionNum") int admissionYear,
+                          @JsonProperty("name") String name,
+                          @JsonProperty("phone") String phone,
+                          @JsonProperty("gender") Gender gender) {
+        this.universityId = universityId;
+        this.admissionYear = admissionYear;
+        this.name = name;
+        this.phone = phone;
+        this.gender = gender;
+    }
 }
