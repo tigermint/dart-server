0a6a92c57df5f94677c51b19412ba32e9a4e3e22
LEESEUNGYEOL <lsy12210@gmail.com>
2023-08-09 16:17:20 +0900

    refactor: [#66] DTO UserResponse 및 mapper 수정 - nickname 추가 - profileImageUrl 추가

--- src/main/java/com/ssh/dartserver/domain/user/dto/mapper/UserMapper.java
+++ src/main/java/com/ssh/dartserver/domain/user/dto/mapper/UserMapper.java
@@ -18,10 +18,12 @@ public interface UserMapper {
     UserWithUniversityResponse toUserWithUniversityResponseDto(UserResponse userResponseDto, UniversityResponse universityResponseDto);
 
     @Mapping(target = "name", source = "user.personalInfo.name.value")
+    @Mapping(target = "nickname", source = "user.personalInfo.nickname.value")
     @Mapping(target = "phone", source = "user.personalInfo.phone.value")
     @Mapping(target = "gender", source = "user.personalInfo.gender.value")
     @Mapping(target = "admissionYear", source = "user.personalInfo.admissionYear.value")
     @Mapping(target = "birthYear", source = "user.personalInfo.birthYear.value")
+    @Mapping(target = "profileImageUrl", source = "user.personalInfo.profileImageUrl.value")
     @Mapping(target = "recommendationCode", source = "user.recommendationCode.value")
     UserResponse toUserResponseDto(User user);
 
