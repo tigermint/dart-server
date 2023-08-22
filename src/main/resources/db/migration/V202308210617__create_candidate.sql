70510e12b6a6aac5dbedb64e8bb2d44cd7e46fd6
LEESEUNGYEOL <lsy12210@gmail.com>
2023-08-22 18:50:27 +0900

    fix: DB migration candidate 테이블 수정 - pk 값 auto increment로 수정

--- src/main/resources/db/migration/V202308210617__create_candidate.sql
+++ src/main/resources/db/migration/V202308210617__create_candidate.sql
@@ -1,6 +1,6 @@
 create table if not exists candidate
 (
-    candidate_id bigint not null
+    candidate_id bigint not null auto_increment
         primary key,
     user_id      bigint null,
     vote_id      bigint null
