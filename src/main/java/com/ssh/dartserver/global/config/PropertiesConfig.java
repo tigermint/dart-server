89f695c7f2ceb13e8e2ccdfcbcfb00621df9b491
Basak <chgy2131@naver.com>
2023-08-30 18:22:01 +0900

    feat: [#67] Slack 환경변수를 불러오는 기능 추가

--- src/main/java/com/ssh/dartserver/global/config/PropertiesConfig.java
+++ src/main/java/com/ssh/dartserver/global/config/PropertiesConfig.java
@@ -1,10 +1,11 @@
 package com.ssh.dartserver.global.config;
 
+import com.ssh.dartserver.global.config.properties.SlackProperty;
 import com.ssh.dartserver.global.config.properties.SwaggerProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(value = {SwaggerProperty.class})
+@EnableConfigurationProperties(value = {SwaggerProperty.class, SlackProperty.class})
 public class PropertiesConfig {
 }
