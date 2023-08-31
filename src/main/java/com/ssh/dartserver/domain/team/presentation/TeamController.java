dc16556106178f9c40e96cdba77cdc8a4ee3abc5
Basak <chgy2131@naver.com>
2023-08-31 18:45:26 +0900

    chore: TeamController, RegionController를 RestController로 명명변경

--- src/main/java/com/ssh/dartserver/domain/team/presentation/TeamController.java
+++ src/main/java/com/ssh/dartserver/domain/team/presentation/TeamController.java
@@ -8,15 +8,15 @@ import com.ssh.dartserver.global.auth.service.oauth.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/teams")
 public class TeamController {
