56baa55ae0edb79c01160f4c06c697f01eb31a82
LEESEUNGYEOL <lsy12210@gmail.com>
2023-09-03 15:10:10 +0900

    feat: [#100] 동시성 제어를 위한 Pessimistic Locking 추가

--- src/main/java/com/ssh/dartserver/domain/user/infra/UserRepository.java
+++ src/main/java/com/ssh/dartserver/domain/user/infra/UserRepository.java
@@ -3,20 +3,24 @@ package com.ssh.dartserver.domain.user.infra;
 import com.ssh.dartserver.domain.user.domain.User;
 import com.ssh.dartserver.domain.user.domain.recommendcode.RecommendationCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query(value = "SELECT u FROM User u WHERE u.id = :userId")
+    Optional<User> findByIdForUpdate(@Param("userId") Long userId);
+
     Optional<User> findByUsername(String username);
 
     Optional<User> findByRecommendationCode(RecommendationCode recommendationCode);
-
-    Optional<User> findById(Long id);
 
     @Query(value = "SELECT u FROM User u join fetch u.university WHERE u.id IN :ids")
     List<User> findAllByIdIn(@Param("ids") List<Long> ids);
@@ -31,4 +35,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
             "FROM Friend f1, Friend f2 " +
             "WHERE f1.user.id = :userId AND f1.friendUser.id = f2.user.id AND f2.friendUser.id != :userId")
     List<User> findAllFriendsOfFriendsByUserId(@Param("userId") Long id);
+
 }
