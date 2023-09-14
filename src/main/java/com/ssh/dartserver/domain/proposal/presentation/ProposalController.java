cdf500bee38bacf27ec7418c82b1c8e519fe5d0f
LEESEUNGYEOL <lsy12210@gmail.com>
2023-09-14 22:23:53 +0900

    feat: [#118] Proposal Endpoint 추가

---
+++ src/main/java/com/ssh/dartserver/domain/proposal/presentation/ProposalController.java
@@ -0,0 +1,32 @@
+package com.ssh.dartserver.domain.proposal.presentation;
+
+import com.ssh.dartserver.domain.proposal.dto.ProposalRequest;
+import com.ssh.dartserver.domain.proposal.service.ProposalService;
+import com.ssh.dartserver.global.auth.service.oauth.PrincipalDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/proposals")
+public class ProposalController {
+    private final ProposalService proposalService;
+    @PostMapping
+    public ResponseEntity<Void> createProposal(Authentication authentication, @RequestBody ProposalRequest.Create request) {
+        PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
+        Long proposalId = proposalService.createProposal(principal.getUser(), request);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(proposalId)
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
