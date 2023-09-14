aa3f0361a1427e2d017c88146cd1fd391598eb64
LEESEUNGYEOL <lsy12210@gmail.com>
2023-09-14 22:47:41 +0900

    feat: [#118] ChatRoom endpoint 추가

---
+++ src/main/java/com/ssh/dartserver/domain/chat/presentation/ChatRoomController.java
@@ -0,0 +1,31 @@
+package com.ssh.dartserver.domain.chat.presentation;
+
+import com.ssh.dartserver.domain.chat.dto.ChatRoomRequest;
+import com.ssh.dartserver.domain.chat.dto.ChatRoomResponse;
+import com.ssh.dartserver.domain.chat.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/chat/rooms")
+public class ChatRoomController {
+
+    private final ChatRoomService chatRoomService;
+    @PostMapping
+    public ResponseEntity<ChatRoomResponse> createRoom(@RequestBody ChatRoomRequest.Create request) {
+        Long chatRoomId = chatRoomService.createChatRoom(request);
+        URI location = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/{id}")
+                .buildAndExpand(chatRoomId)
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
