8e01bf021cc92e147f1357b007b46a13d6e11da3
LEESEUNGYEOL <lsy12210@gmail.com>
2023-09-14 22:19:26 +0900

    feat: [#118] Proposal Dto 추가

---
+++ src/main/java/com/ssh/dartserver/domain/proposal/dto/ProposalRequest.java
@@ -0,0 +1,21 @@
+package com.ssh.dartserver.domain.proposal.dto;
+
+import lombok.Data;
+
+public class ProposalRequest {
+    private ProposalRequest() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    @Data
+    public static class Create {
+        private Long requestingTeamId;
+        private Long requestedTeamId;
+    }
+
+    @Data
+    public static class Update{
+        private String proposalStatus;
+    }
+
+}
