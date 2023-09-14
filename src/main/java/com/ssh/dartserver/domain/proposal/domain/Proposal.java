d167a40995dbd0e344300bdc98cc750beb595c51
LEESEUNGYEOL <lsy12210@gmail.com>
2023-09-14 21:58:59 +0900

    feat: [#118] proposal 도메인 추가

---
+++ src/main/java/com/ssh/dartserver/domain/proposal/domain/Proposal.java
@@ -0,0 +1,34 @@
+package com.ssh.dartserver.domain.proposal.domain;
+
+import com.ssh.dartserver.domain.team.domain.Team;
+import com.ssh.dartserver.global.common.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Proposal extends BaseTimeEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "proposal_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "proposal_status")
+    private ProposalStatus proposalStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "requesting_team_id")
+    private Team requestingTeam;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "requested_team_id")
+    private Team requestedTeam;
+
+    public void updateProposalStatus(ProposalStatus proposalStatus) {
+        this.proposalStatus = proposalStatus;
+    }
+}
---
+++ src/main/java/com/ssh/dartserver/domain/proposal/domain/ProposalStatus.java
@@ -0,0 +1,4 @@
+package com.ssh.dartserver.domain.proposal.domain;
+public enum ProposalStatus {
+    PROPOSAL_IN_PROGRESS, PROPOSAL_SUCCESS, PROPOSAL_FAILED;
+}
