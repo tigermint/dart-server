634ad2e0ffd9c87507703cde2dbf3dc0c8d2f393
LEESEUNGYEOL <lsy12210@gmail.com>
2023-09-15 14:32:54 +0900

    feat: [#118] chatRoomUserRepository 삭제 로직 추가

--- src/main/java/com/ssh/dartserver/domain/chat/presentation/ChatRoomUserRepository.java
+++ src/main/java/com/ssh/dartserver/domain/chat/presentation/ChatRoomUserRepository.java
@@ -5,11 +5,11 @@ import com.ssh.dartserver.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ChatRoomUserRepository extends JpaRepository<ChatRoomUser, Long> {
-    Optional<ChatRoomUser> findByChatRoomIdAndUserId(Long chatRoomId, Long userId);
     List<ChatRoomUser> findAllByUser(User user);
 
     List<ChatRoomUser> findAllByChatRoomId(Long chatRoomId);
+
+    void deleteByUserIdAndChatRoomId(Long userId, Long chatRoomId);
 }
