dafc11df3984b97e885ea55dd97d760a275db2d3
LEESEUNGYEOL <lsy12210@gmail.com>
2023-09-20 11:16:21 +0900

    feat: [#119] SingleTeamFriend 도메인 추가

---
+++ src/main/java/com/ssh/dartserver/domain/team/domain/SingleTeamFriend.java
@@ -0,0 +1,44 @@
+package com.ssh.dartserver.domain.team.domain;
+
+import com.ssh.dartserver.domain.university.domain.University;
+import com.ssh.dartserver.domain.user.domain.personalinfo.BirthYear;
+import com.ssh.dartserver.domain.user.domain.personalinfo.Nickname;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class SingleTeamFriend {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "single_team_friend_profile_id")
+    private Long id;
+
+    @Embedded
+    private Nickname nickname;
+
+    @Embedded
+    private BirthYear birthYear;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Team team;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "university_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private University university;
+
+    @Builder
+    public SingleTeamFriend(String nickname, int birthYear, String profileImageUrl, Team team, University university) {
+        this.nickname = Nickname.from(nickname);
+        this.birthYear = BirthYear.from(birthYear);
+        this.profileImageUrl = profileImageUrl;
+        this.team = team;
+        this.university = university;
+    }
+}
