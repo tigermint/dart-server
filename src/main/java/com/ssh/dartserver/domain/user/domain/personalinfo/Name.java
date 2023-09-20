51139d83a0a4546afd52823dd1036939258ae3c6
LEESEUNGYEOL <lsy12210@gmail.com>
2023-09-20 11:12:33 +0900

    feat: [#119] User 도메인 VO validation 추가 - Nickname - Name - BirthYear

--- src/main/java/com/ssh/dartserver/domain/user/domain/personalinfo/Name.java
+++ src/main/java/com/ssh/dartserver/domain/user/domain/personalinfo/Name.java
@@ -14,7 +14,14 @@ public class Name {
     private String value;
 
     public Name(String value) {
+        validateLength(value);
         this.value = value;
+    }
+
+    private void validateLength(String value) {
+        if (value.length() > 4) {
+            throw new IllegalArgumentException("이름은 4글자 이하만 가능합니다");
+        }
     }
 
     public static Name from(String value) {
--- src/main/java/com/ssh/dartserver/domain/user/domain/personalinfo/Nickname.java
+++ src/main/java/com/ssh/dartserver/domain/user/domain/personalinfo/Nickname.java
@@ -14,7 +14,14 @@ public class Nickname {
     private String value;
 
     public Nickname(String value) {
+        validateLength(value);
         this.value = value;
+    }
+
+    private void validateLength(String value) {
+        if (value.length() < 2 || value.length() > 10) {
+            throw new IllegalArgumentException("닉네임은 2자 이상 10자 이하로 입력해주세요.");
+        }
     }
 
     public static Nickname from(String value) {
