a659bf80bd797d87cbf97f79159122018fef9906
LEESEUNGYEOL <lsy12210@gmail.com>
2023-09-23 02:49:45 +0900

    feat: [#119] ChatRoomUserRepository findAllByTeamId 추가

--- src/main/java/com/ssh/dartserver/domain/chat/presentation/ChatRoomUserRepository.java
+++ src/main/java/com/ssh/dartserver/domain/chat/presentation/ChatRoomUserRepository.java
@@ -3,6 +3,8 @@ package com.ssh.dartserver.domain.chat.presentation;
 import com.ssh.dartserver.domain.chat.domain.ChatRoomUser;
 import com.ssh.dartserver.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,6 +12,13 @@ public interface ChatRoomUserRepository extends JpaRepository<ChatRoomUser, Long
     List<ChatRoomUser> findAllByUser(User user);
 
     List<ChatRoomUser> findAllByChatRoomId(Long chatRoomId);
+
+    @Query("select cru " +
+            "from ChatRoomUser cru " +
+            "join fetch cru.chatRoom cr " +
+            "join fetch cr.proposal p " +
+            "where p.requestingTeam.id = :teamId or p.requestedTeam.id = :teamId")
+    List<ChatRoomUser> findAllByTeamId(@Param("teamId") Long teamId);
 
     void deleteByUserIdAndChatRoomId(Long userId, Long chatRoomId);
 }
