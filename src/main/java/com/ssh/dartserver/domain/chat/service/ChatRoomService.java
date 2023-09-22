b4a941ccb2c76e1f0363c132a270b9a96201a091
LEESEUNGYEOL <lsy12210@gmail.com>
2023-09-23 02:27:03 +0900

    feat: [#119] ChatRoomService SingleTeam의 경우 조회 가능하도록 반환 형태 로직 추가

--- src/main/java/com/ssh/dartserver/domain/chat/service/ChatRoomService.java
+++ src/main/java/com/ssh/dartserver/domain/chat/service/ChatRoomService.java
@@ -8,31 +8,46 @@ import com.ssh.dartserver.domain.chat.dto.mapper.ChatRoomMapper;
 import com.ssh.dartserver.domain.chat.infra.ChatRoomRepository;
 import com.ssh.dartserver.domain.chat.presentation.ChatRoomUserRepository;
 import com.ssh.dartserver.domain.proposal.domain.Proposal;
+import com.ssh.dartserver.domain.proposal.domain.ProposalStatus;
 import com.ssh.dartserver.domain.proposal.infra.ProposalRepository;
 import com.ssh.dartserver.domain.team.domain.Team;
 import com.ssh.dartserver.domain.team.domain.TeamRegion;
 import com.ssh.dartserver.domain.team.domain.TeamUser;
+import com.ssh.dartserver.domain.team.infra.SingleTeamFriendRepository;
 import com.ssh.dartserver.domain.team.infra.TeamRegionRepository;
 import com.ssh.dartserver.domain.team.infra.TeamUserRepository;
 import com.ssh.dartserver.domain.user.domain.User;
+import com.ssh.dartserver.domain.user.domain.profilequestions.ProfileQuestions;
 import com.ssh.dartserver.domain.user.domain.studentverificationinfo.StudentIdCardVerificationStatus;
 import com.ssh.dartserver.domain.user.domain.studentverificationinfo.StudentVerificationInfo;
+import com.ssh.dartserver.global.infra.notification.PlatformNotification;
+import com.ssh.dartserver.global.util.TeamAverageAgeCalculator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ChatRoomService {
+    private static final String CHAT_ROOM_OPEN_CONTENTS = "채팅방이 열렸어요, 과팅 약속 잡아봐요 😀";
+
     private final ChatRoomRepository chatRoomRepository;
     private final ChatRoomUserRepository chatRoomUserRepository;
     private final ProposalRepository proposalRepository;
     private final TeamUserRepository teamUserRepository;
     private final TeamRegionRepository teamRegionRepository;
+    private final SingleTeamFriendRepository singleTeamFriendRepository;
+    private final TeamAverageAgeCalculator teamAverageAgeCalculator;
+
+    private final PlatformNotification notification;
 
     private final ChatRoomMapper chatRoomMapper;
 
@@ -63,8 +78,18 @@ public class ChatRoomService {
                         .build())
                 .collect(Collectors.toList());
 
+        proposal.updateProposalStatus(ProposalStatus.PROPOSAL_SUCCESS);
         chatRoomRepository.save(chatRoom);
         chatRoomUserRepository.saveAll(chatRoomUsers);
+
+        List<Long> chatRoomUserIds = chatRoomUsers.stream()
+                .map(ChatRoomUser::getUser)
+                .map(User::getId)
+                .collect(Collectors.toList());
+
+        CompletableFuture.runAsync(() ->
+                notification.postNotificationSpecificDevice(chatRoomUserIds, CHAT_ROOM_OPEN_CONTENTS)
+        );
         return chatRoom.getId();
     }
 
@@ -133,30 +158,15 @@ public class ChatRoomService {
     }
 
 
-    private static boolean isStudentIdCardVerified(List<TeamUser> requestedTeamUsers) {
-        return requestedTeamUsers.stream()
-                .map(TeamUser::getUser)
-                .map(User::getStudentVerificationInfo)
-                .map(StudentVerificationInfo::getStudentIdCardVerificationStatus)
-                .anyMatch(predicate -> predicate == StudentIdCardVerificationStatus.VERIFICATION_SUCCESS);
-    }
-
     private ChatRoomResponse.ReadDto.TeamDto getReadTeamDto(Team team, List<TeamUser> teamUsers, List<TeamRegion> teamRegions) {
         return chatRoomMapper.toReadTeamDto(
                 team,
                 isStudentIdCardVerified(teamUsers),
-                teamUsers.stream()
-                        .map(TeamUser::getUser)
-                        .map(user -> chatRoomMapper.toReadUserDto(
-                                user,
-                                chatRoomMapper.toReadUniversityDto(user.getUniversity()),
-                                user.getProfileQuestions().getValues().stream()
-                                        .map(profileQuestion -> chatRoomMapper.toReadProfileQuestionDto(
-                                                        profileQuestion,
-                                                        chatRoomMapper.toReadQuestionDto(profileQuestion.getQuestion())
-                                                )
-                                        ).collect(Collectors.toList())))
-                        .collect(Collectors.toList()),
+                teamAverageAgeCalculator.getAverageAge(teamUsers),
+                Optional.of(teamUsers)
+                        .filter(users -> users.size() == 1)
+                        .map(users -> getReadSingleTeamUserDto(team, users))
+                        .orElseGet(() -> getReadMultipleTeamUserDto(teamUsers)),
                 teamRegions.stream()
                         .map(TeamRegion::getRegion)
                         .map(chatRoomMapper::toReadRegionDto)
@@ -164,21 +174,96 @@ public class ChatRoomService {
         );
     }
 
+
     private ChatRoomResponse.ListDto.TeamDto getListTeamDto(Team team, List<TeamUser> teamUsers, List<TeamRegion> teamRegions) {
         return chatRoomMapper.toListTeamDto(
                 team,
                 isStudentIdCardVerified(teamUsers),
                 chatRoomMapper.toListUniversityDto(team.getUniversity()),
-                teamUsers.stream()
-                        .map(TeamUser::getUser)
-                        .map(chatRoomMapper::toListUserDto)
-                        .collect(Collectors.toList()),
+                Optional.of(teamUsers)
+                        .filter(users -> users.size() == 1)
+                        .map(users -> getListSingleTeamUserDto(team, users))
+                        .orElseGet(() -> getListMultipleTeamUserDto(teamUsers)),
                 teamRegions.stream()
                         .map(TeamRegion::getRegion)
                         .map(chatRoomMapper::toListRegionDto)
                         .collect(Collectors.toList())
         );
     }
+
+    private List<ChatRoomResponse.ReadDto.UserDto> getReadSingleTeamUserDto(Team team, List<TeamUser> teamUsers) {
+        return Stream.concat(
+                        teamUsers.stream()
+                                .map(TeamUser::getUser),
+                        singleTeamFriendRepository.findAllByTeam(team).stream()
+                                .map(singleTeamFriend ->
+                                        User.createSingleTeamFriendUser(
+                                                singleTeamFriend.getNickname().getValue(),
+                                                singleTeamFriend.getBirthYear().getValue(),
+                                                singleTeamFriend.getProfileImageUrl().getValue(),
+                                                singleTeamFriend.getUniversity()
+                                        )
+                                )
+                )
+                .map(teamUser ->
+                        chatRoomMapper.toReadUserDto(
+                                teamUser,
+                                chatRoomMapper.toReadUniversityDto(teamUser.getUniversity()),
+                                Optional.ofNullable(teamUser.getProfileQuestions())
+                                        .map(ProfileQuestions::getValues)
+                                        .orElse(Collections.emptyList())
+                                        .stream()
+                                        .map(profileQuestion -> chatRoomMapper.toReadProfileQuestionDto(
+                                                profileQuestion,
+                                                chatRoomMapper.toReadQuestionDto(profileQuestion.getQuestion())
+                                        ))
+                                        .collect(Collectors.toList())
+                        )
+                )
+                .collect(Collectors.toList());
+    }
+
+    private List<ChatRoomResponse.ReadDto.UserDto> getReadMultipleTeamUserDto(List<TeamUser> teamUsers) {
+        return teamUsers.stream()
+                .map(TeamUser::getUser)
+                .map(user -> chatRoomMapper.toReadUserDto(
+                        user,
+                        chatRoomMapper.toReadUniversityDto(user.getUniversity()),
+                        user.getProfileQuestions().getValues().stream()
+                                .map(profileQuestion -> chatRoomMapper.toReadProfileQuestionDto(
+                                                profileQuestion,
+                                                chatRoomMapper.toReadQuestionDto(profileQuestion.getQuestion())
+                                        )
+                                ).collect(Collectors.toList())))
+                .collect(Collectors.toList());
+    }
+
+    private List<ChatRoomResponse.ListDto.UserDto> getListSingleTeamUserDto(Team team, List<TeamUser> users) {
+        return Stream.concat(
+                        users.stream()
+                                .map(TeamUser::getUser),
+                        singleTeamFriendRepository.findAllByTeam(team).stream()
+                                .map(singleTeamFriend ->
+                                        User.createSingleTeamFriendUser(
+                                                singleTeamFriend.getNickname().getValue(),
+                                                singleTeamFriend.getBirthYear().getValue(),
+                                                singleTeamFriend.getProfileImageUrl().getValue(),
+                                                singleTeamFriend.getUniversity()
+                                        )
+                                )
+                )
+                .map(chatRoomMapper::toListUserDto)
+                .collect(Collectors.toList());
+
+    }
+
+    private List<ChatRoomResponse.ListDto.UserDto> getListMultipleTeamUserDto(List<TeamUser> teamUsers) {
+        return teamUsers.stream()
+                .map(TeamUser::getUser)
+                .map(chatRoomMapper::toListUserDto)
+                .collect(Collectors.toList());
+    }
+
     private static List<TeamUser> getTeamUsersInChatRoom(List<ChatRoomUser> chatRoomUsers, List<TeamUser> requestingTeamUsers) {
         List<Long> chatRoomUserIds = chatRoomUsers.stream()
                 .map(ChatRoomUser::getUser)
@@ -189,12 +274,23 @@ public class ChatRoomService {
                 .collect(Collectors.toList());
     }
 
+    private static boolean isStudentIdCardVerified(List<TeamUser> requestedTeamUsers) {
+        return requestedTeamUsers.stream()
+                .map(TeamUser::getUser)
+                .map(User::getStudentVerificationInfo)
+                .map(StudentVerificationInfo::getStudentIdCardVerificationStatus)
+                .anyMatch(predicate -> predicate == StudentIdCardVerificationStatus.VERIFICATION_SUCCESS);
+    }
+
+
     private void validateMeetStatus(Proposal proposal) {
         if (proposal.getProposalStatus().isSuccess()) {
-            throw new IllegalArgumentException("완료되지 않은 매칭입니다.");
+            throw new IllegalArgumentException("이미 성공한 매칭입니다.");
         }
         if (proposal.getProposalStatus().isFailed()) {
             throw new IllegalArgumentException("실패한 매칭입니다.");
         }
     }
+
+
 }
