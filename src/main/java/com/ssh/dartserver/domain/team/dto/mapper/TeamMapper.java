ec2a7144dfa0ed187224e6b2ee0262cf4d571620
LEESEUNGYEOL <lsy12210@gmail.com>
2023-09-23 02:46:59 +0900

    feat: [#119] 팀 조회 mapper averageBirthYear를 averageAge 로 수정

--- src/main/java/com/ssh/dartserver/domain/team/dto/mapper/TeamMapper.java
+++ src/main/java/com/ssh/dartserver/domain/team/dto/mapper/TeamMapper.java
@@ -7,6 +7,8 @@ import com.ssh.dartserver.domain.team.dto.TeamResponse;
 import com.ssh.dartserver.domain.team.dto.BlindDateUserDetailResponse;
 import com.ssh.dartserver.domain.team.dto.BlindDateUserResponse;
 import com.ssh.dartserver.domain.team.dto.BlindDateTeamResponse;
+import com.ssh.dartserver.domain.user.domain.User;
+import com.ssh.dartserver.domain.user.dto.ProfileQuestionResponse;
 import com.ssh.dartserver.domain.user.dto.UserProfileResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -24,19 +26,36 @@ public interface TeamMapper {
 
     @Mapping(target = "id", source = "team.id")
     @Mapping(target = "name", source = "team.name.value")
-    @Mapping(target = "averageBirthYear", source = "averageBirthYear")
+    @Mapping(target = "averageAge", source = "averageAge")
     @Mapping(target = "regions", source = "teamRegions")
     @Mapping(target = "universityName", source = "universityName")
     @Mapping(target = "isCertifiedTeam", source = "isCertifiedTeam")
     @Mapping(target = "teamUsers", source = "teamUsers")
-    BlindDateTeamResponse toBlindDateTeamResponse(Team team, double averageBirthYear, List<RegionResponse> teamRegions, String universityName, boolean isCertifiedTeam, List<BlindDateUserResponse> teamUsers);
+    BlindDateTeamResponse toBlindDateTeamResponse(Team team, Double averageAge, List<RegionResponse> teamRegions, String universityName, Boolean isCertifiedTeam, List<BlindDateUserResponse> teamUsers);
 
     @Mapping(target = "id", source = "team.id")
     @Mapping(target = "name", source = "team.name.value")
-    @Mapping(target = "averageBirthYear", source = "averageBirthYear")
+    @Mapping(target = "averageAge", source = "averageAge")
     @Mapping(target = "regions", source = "teamRegions")
     @Mapping(target = "universityName", source = "universityName")
     @Mapping(target = "isCertifiedTeam", source = "isCertifiedTeam")
     @Mapping(target = "teamUsers", source = "teamUsers")
-    BlindDateTeamDetailResponse toBlindDateTeamDetailResponse(Team team, double averageBirthYear, List<RegionResponse> teamRegions, String universityName, boolean isCertifiedTeam, List<BlindDateUserDetailResponse> teamUsers);
+    @Mapping(target = "isAlreadyProposalTeam", source = "isAlreadyProposalTeam")
+    BlindDateTeamDetailResponse toBlindDateTeamDetailResponse(Team team, Double averageAge, List<RegionResponse> teamRegions, String universityName, Boolean isCertifiedTeam, List<BlindDateUserDetailResponse> teamUsers, Boolean isAlreadyProposalTeam);
+
+    @Mapping(target = "id", source = "user.id")
+    @Mapping(target = "name", source = "nicknameOrName")
+    @Mapping(target = "profileImageUrl", source = "user.personalInfo.profileImageUrl.value")
+    @Mapping(target = "department", source = "user.university.department")
+    @Mapping(target = "isCertifiedUser", source = "isCertifiedUser")
+    @Mapping(target = "birthYear", source = "user.personalInfo.birthYear.value")
+    @Mapping(target = "profileQuestionResponses", source = "profileQuestionResponses")
+    BlindDateUserDetailResponse toBlindDateUserDetailResponse(User user, Boolean isCertifiedUser, String nicknameOrName, List<ProfileQuestionResponse> profileQuestionResponses);
+
+
+    @Mapping(target = "id", source = "user.id")
+    @Mapping(target = "name", source = "nicknameOrName")
+    @Mapping(target = "profileImageUrl", source = "user.personalInfo.profileImageUrl.value")
+    @Mapping(target = "department", source = "user.university.department")
+    BlindDateUserResponse toBlindDateUserResponse(User user, String nicknameOrName);
 }
