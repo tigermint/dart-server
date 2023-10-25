d7e0cf814b268c8f9b0791d8264000b4a8200380
LEESEUNGYEOL <lsy12210@gmail.com>
2023-10-25 18:14:32 +0900

    feat: [#163] findAllVisibleTeam 필터링, 정렬 로직 추가 - 정렬: 최신순, 호감순, 조회순 - 필터: 프로필 등록 유무, 학생증 인증 유무, 지역

--- src/main/java/com/ssh/dartserver/domain/team/infra/TeamRepositoryImpl.java
+++ src/main/java/com/ssh/dartserver/domain/team/infra/TeamRepositoryImpl.java
@@ -2,91 +2,123 @@ package com.ssh.dartserver.domain.team.infra;
 
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.ssh.dartserver.domain.team.domain.QTeam;
 import com.ssh.dartserver.domain.team.domain.Team;
-import com.ssh.dartserver.domain.user.domain.personalinfo.Gender;
+import com.ssh.dartserver.domain.team.dto.OrderMethod;
+import com.ssh.dartserver.domain.team.dto.TeamSearchCondition;
+import com.ssh.dartserver.domain.user.domain.User;
+import com.ssh.dartserver.domain.user.domain.studentverificationinfo.StudentIdCardVerificationStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static com.ssh.dartserver.domain.proposal.domain.QProposal.proposal;
+import static com.ssh.dartserver.domain.team.domain.QSingleTeamFriend.singleTeamFriend;
+import static com.ssh.dartserver.domain.team.domain.QTeam.team;
 import static com.ssh.dartserver.domain.team.domain.QTeamRegion.teamRegion;
 import static com.ssh.dartserver.domain.team.domain.QTeamUser.teamUser;
+import static com.ssh.dartserver.domain.user.domain.QUser.user;
 
 @RequiredArgsConstructor
 public class TeamRepositoryImpl implements TeamRepositoryCustom {
+    private static final int ALL_REGION = 0;
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Page<Team> findAllVisibleTeams(Long myUniversityId, Gender myGender, Pageable pageable) {
-        Predicate predicate = teamUser.team.isVisibleToSameUniversity.isTrue()
-                .or(
-                        teamUser.team.isVisibleToSameUniversity.isFalse()
-                                .and(teamUser.team.university.id.ne(myUniversityId))
-                )
-                .and(teamUser.user.personalInfo.gender.ne(myGender));
-
-        List<Long> teamIds = queryFactory
-                .select(teamUser.team.id)
+    public Page<Team> findAllVisibleTeam(User currentUser, TeamSearchCondition condition, Pageable pageable) {
+        List<Team> content = queryFactory
+                .select(team)
                 .distinct()
-                .from(teamUser)
-                .join(teamRegion).on(teamUser.team.eq(teamRegion.team))
-                .where(predicate)
+                .from(team)
+                .join(team.teamUsers, teamUser)
+                .join(team.teamRegions, teamRegion)
+                .join(teamUser.user, user)
+                .join(team.singleTeamFriends, singleTeamFriend)
+                .leftJoin(team.requestedTeamProposals, proposal)
+                .where(
+                        teamUser.user.personalInfo.gender.ne(currentUser.getPersonalInfo().getGender()),
+                        isVisibleToSameUniversityEq(currentUser.getUniversity().getId()),
+                        verifiedStudentEq(condition.getVerifiedStudent()),
+                        hasProfileImageEq(condition.getHasProfileImage()),
+                        regionEq(condition.getRegionId())
+                )
+                .orderBy(createOrderSpecifier(condition.getOrder()))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        List<Team> teams = queryFactory.selectFrom(QTeam.team)
-                .where(QTeam.team.id.in(teamIds))
-                .orderBy(new OrderSpecifier(Order.DESC, QTeam.team.id))
-                .fetch();
-
-        long total = queryFactory
-                .selectDistinct(teamUser.team)
-                .from(teamUser)
-                .join(teamRegion).on(teamUser.team.eq(teamRegion.team))
-                .where(predicate)
-                .fetchCount();
-
-        return new PageImpl<>(teams, pageable, total);
+        JPAQuery<Long> countQuery = queryFactory
+                .select(team.countDistinct())
+                .from(team)
+                .join(team.teamUsers, teamUser)
+                .join(team.teamRegions, teamRegion)
+                .join(teamUser.user, user)
+                .join(team.singleTeamFriends, singleTeamFriend)
+                .where(
+                        teamUser.user.personalInfo.gender.ne(currentUser.getPersonalInfo().getGender()),
+                        isVisibleToSameUniversityEq(currentUser.getUniversity().getId()),
+                        verifiedStudentEq(condition.getVerifiedStudent()),
+                        hasProfileImageEq(condition.getHasProfileImage()),
+                        regionEq(condition.getRegionId())
+                );
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
     }
 
-    @Override
-    public Page<Team> findAllVisibleTeamsByRegionId(Long myUniversityId, Gender myGender, Long regionId, Pageable pageable) {
-        Predicate predicate = teamUser.team.isVisibleToSameUniversity.isTrue()
-                .or(
-                        teamUser.team.isVisibleToSameUniversity.isFalse()
-                                .and(teamUser.team.university.id.ne(myUniversityId))
-                )
-                .and(teamUser.user.personalInfo.gender.ne(myGender))
-                .and(teamRegion.region.id.eq(regionId));
+    private static OrderSpecifier<?>[] createOrderSpecifier(OrderMethod order) {
+        List<OrderSpecifier<?>> orderSpecifiers = new ArrayList<>();
 
-        List<Long> teamIds = queryFactory
-                .select(teamUser.team.id)
-                .distinct()
-                .from(teamUser)
-                .join(teamRegion).on(teamUser.team.eq(teamRegion.team))
-                .where(predicate)
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetch();
+        if (order == null || order.equals(OrderMethod.LATEST))
+            orderSpecifiers.add(new OrderSpecifier<>(Order.DESC, team.createdTime));
+        else if (order.equals(OrderMethod.LIKE)) {
+            orderSpecifiers.add(new OrderSpecifier<>(Order.DESC, team.requestedTeamProposals.size()));
+            orderSpecifiers.add(new OrderSpecifier<>(Order.DESC, team.createdTime));
+        } else if (order.equals(OrderMethod.VIEW)) {
+            orderSpecifiers.add(new OrderSpecifier<>(Order.DESC, team.viewCount.value));
+        } else {
+            orderSpecifiers.add(new OrderSpecifier<>(Order.DESC, team.createdTime));
+        }
+        return orderSpecifiers.toArray(new OrderSpecifier[0]);
+    }
 
-        List<Team> teams = queryFactory.selectFrom(QTeam.team)
-                .where(QTeam.team.id.in(teamIds))
-                .orderBy(new OrderSpecifier(Order.DESC, QTeam.team.id))
-                .fetch();
+    private BooleanExpression hasProfileImageEq(Boolean hasProfileImage) {
+        if (hasProfileImage != null) {
+            return hasProfileImage.equals(Boolean.TRUE)
+                    ? user.personalInfo.profileImageUrl.value.ne("DEFAULT")
+                    .or(singleTeamFriend.profileImageUrl.value.ne("DEFAULT"))
+                    : null;
+        }
+        return null;
+    }
 
-        long total = queryFactory
-                .selectDistinct(teamUser.team)
-                .from(teamUser)
-                .join(teamRegion).on(teamUser.team.eq(teamRegion.team))
-                .where(predicate)
-                .fetchCount();
+    private BooleanExpression isVisibleToSameUniversityEq(Long universityId) {
+        return team.isVisibleToSameUniversity.isTrue()
+                .or(team.isVisibleToSameUniversity.isFalse().and(team.university.id.ne(universityId)));
+    }
 
-        return new PageImpl<>(teams, pageable, total);
+    private BooleanExpression verifiedStudentEq(Boolean verifiedStudent) {
+        if (verifiedStudent != null) {
+            return verifiedStudent.equals(Boolean.TRUE)
+                    ? user.studentVerificationInfo.studentIdCardVerificationStatus.eq(StudentIdCardVerificationStatus.VERIFICATION_SUCCESS)
+                    : null;
+        }
+        return null;
+    }
+
+    //TODO: 전체 지역 로직 수정 필요
+    private BooleanExpression regionEq(Long regionId) {
+        if (regionId != null) {
+            if (regionId == ALL_REGION) {
+                return null;
+            }else{
+                return teamRegion.region.id.eq(regionId);
+            }
+        }
+        return null;
     }
 }
