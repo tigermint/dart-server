69d5a23f8b714b7cf9ef66900ca1e73ee694b9bc
LEESEUNGYEOL <lsy12210@gmail.com>
2023-11-07 23:47:26 +0900

    feat: [#181] CommentRepository 추가

---
+++ src/main/java/com/ssh/dartserver/domain/survey/infra/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.ssh.dartserver.domain.survey.infra;
+
+import com.ssh.dartserver.domain.survey.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findByIdAndSurveyId(Long commentId, Long surveyId);
+}
