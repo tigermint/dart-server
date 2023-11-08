9ae223f3ba139c33ef235fd4b666f1f66e9b5e88
LEESEUNGYEOL <lsy12210@gmail.com>
2023-11-08 17:12:25 +0900

    feat: [#181] 회원 탈퇴 시 Survey 관련 회원 값 null로 수정하는 로직 추가 - comment - answerUser - commentLike - commentReport

--- src/main/java/com/ssh/dartserver/domain/survey/infra/CommentRepository.java
+++ src/main/java/com/ssh/dartserver/domain/survey/infra/CommentRepository.java
@@ -1,10 +1,18 @@
 package com.ssh.dartserver.domain.survey.infra;
 
 import com.ssh.dartserver.domain.survey.domain.Comment;
+import com.ssh.dartserver.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Optional<Comment> findByIdAndSurveyId(Long commentId, Long surveyId);
+
+    @Modifying
+    @Query("update Comment c set c.user = null where c.user = :user")
+    void updateAllUserToNull(@Param("user") User user);
 }
