fec2bdaae4da005572897268c8fa0edde28c3f54
Basak <chgy2131@naver.com>
2024-05-21 20:11:13 +0900

    feat: [#194] 통합테스트를 위한 기본 기능 추가

---
+++ src/test/java/com/ssh/dartserver/health/HealthApiTest.java
@@ -0,0 +1,28 @@
+package com.ssh.dartserver.health;
+
+import com.ssh.dartserver.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+public class HealthApiTest extends ApiTest {
+    @Test
+    void 헬스체크() {
+        final ExtractableResponse<Response> response = 헬스체크요청();
+
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    public static ExtractableResponse<Response> 헬스체크요청() {
+        return RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .get("/v1/health")
+            .then()
+            .log().all().extract();
+    }
+}
