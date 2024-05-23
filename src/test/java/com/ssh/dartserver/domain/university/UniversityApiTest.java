30473172508f6e2626ec1b327be75e237260ef5f
Basak <chgy2131@naver.com>
2024-05-23 12:28:37 +0900

    test: [#194] University 대학 목록 조회 관련 테스트 추가

---
+++ src/test/java/com/ssh/dartserver/domain/university/UniversityApiTest.java
@@ -0,0 +1,38 @@
+package com.ssh.dartserver.domain.university;
+
+import static com.ssh.dartserver.domain.university.UniversitySteps.*;
+import static org.assertj.core.api.Assertions.*;
+
+import com.ssh.dartserver.ApiTest;
+import com.ssh.dartserver.UserManager;
+import com.ssh.dartserver.domain.university.domain.University;
+import com.ssh.dartserver.domain.university.dto.UniversityResponse;
+import com.ssh.dartserver.domain.university.infra.UniversityRepository;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+public class UniversityApiTest extends ApiTest {
+    @Autowired
+    private UniversityRepository universityRepository;
+    @Autowired
+    private UserManager userManager;
+
+    @Test
+    void 대학목록확인() {
+        long 생성대학수 = 100L;
+        List<University> universities = 대학생성요청_생성(생성대학수);
+        universityRepository.saveAll(universities);
+        final String jwtToken = userManager.createTestUser().getJwtToken();
+
+        final ExtractableResponse<Response> response = 대학목록조회요청(jwtToken);
+        final UniversityResponse[] universityDtos = response.body().as(UniversityResponse[].class);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(universityDtos.length).isEqualTo(생성대학수);
+    }
+
+}
