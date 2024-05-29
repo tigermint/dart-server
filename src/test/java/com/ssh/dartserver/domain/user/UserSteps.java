fc4c2b9ce58bbd9f63ac2ca152e28c22b9ed0e27
Basak <chgy2131@naver.com>
2024-05-29 15:32:38 +0900

    test: [#194] User 회원탈퇴 및 정보확인 관련 테스트 코드 추가

--- src/test/java/com/ssh/dartserver/domain/user/UserSteps.java
+++ src/test/java/com/ssh/dartserver/domain/user/UserSteps.java
@@ -2,9 +2,11 @@ package com.ssh.dartserver.domain.user;
 
 import com.ssh.dartserver.domain.user.domain.personalinfo.Gender;
 import com.ssh.dartserver.domain.user.dto.UserSignupRequest;
+import com.ssh.dartserver.domain.user.dto.UserUpdateRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.ArrayList;
 import org.springframework.http.MediaType;
 
 public class UserSteps {
@@ -28,5 +30,45 @@ public class UserSteps {
             .phone("01012345678")
             .gender(Gender.MALE)
             .build();
+    }
+
+    public static ExtractableResponse<Response> 내정보확인요청(final String jwtToken) {
+        return RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .auth().oauth2(jwtToken)
+            .when()
+            .get("/v1/users/me")
+            .then()
+            .log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 회원탈퇴요청(final String jwtToken) {
+        return RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .auth().oauth2(jwtToken)
+            .when()
+            .delete("/v1/users/me")
+            .then()
+            .log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 내정보수정요청(final String jwtToken, UserUpdateRequest request) {
+        return RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .auth().oauth2(jwtToken)
+            .body(request)
+            .when()
+            .patch("/v1/users/me")
+            .then()
+            .log().all().extract();
+    }
+
+    public static UserUpdateRequest 내정보수정요청_생성() {
+        UserUpdateRequest request = new UserUpdateRequest();
+        request.setNickname("수정된닉네임");
+        request.setProfileImageUrl("https://via.placeholder.com/200x200");
+        request.setProfileQuestionIds(new ArrayList<>());  // 필요없는걸 빈 리스트로 넣는것이 무슨의미인가! 관련(투표) 코드 제거할때 함께 제거
+
+        return request;
     }
 }
