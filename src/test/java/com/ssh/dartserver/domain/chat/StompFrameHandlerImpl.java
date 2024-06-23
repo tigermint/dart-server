46cac6bb89099269de314f9fd920ddb75d5b1d22
LEESEUNGYEOL <lsy12210@gmail.com>
2024-06-24 00:40:02 +0900

    fix: [#198] StompFrameHandlerImpl.handleFrame 로깅 제거

--- src/test/java/com/ssh/dartserver/domain/chat/StompFrameHandlerImpl.java
+++ src/test/java/com/ssh/dartserver/domain/chat/StompFrameHandlerImpl.java
@@ -23,7 +23,6 @@ public class StompFrameHandlerImpl<T> implements StompFrameHandler {
 
     @Override
     public void handleFrame(final StompHeaders headers, final Object payload) {
-        System.out.println(payload);
         responses.offer((T) payload);
     }
 }
