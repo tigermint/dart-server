c0cfe13196fd56b83189664acb027126d97c1f11
Basak <chgy2131@naver.com>
2024-07-21 19:57:04 +0900

    chore: [#201] OauthServiceAbstract 주석 추가

--- src/main/java/com/ssh/dartserver/global/auth/service/OauthServiceAbstract.java
+++ src/main/java/com/ssh/dartserver/global/auth/service/OauthServiceAbstract.java
@@ -19,6 +19,13 @@ public abstract class OauthServiceAbstract implements OauthService {
     protected final BCryptPasswordEncoder bCryptPasswordEncoder;
     protected final JwtTokenProvider jwtTokenProvider;
 
+    /**
+     * 유저 정보가 있으면 JWT 토큰을 바로 반환하고, 없으면 생성한뒤에 그걸 기반으로 JWT Token을 반환합니다.
+     * @param oauthUser OAuth 유저 정보
+     * @param userEntity 저장소에서 조회한 유저 정보
+     * @return
+     */
+    // FIXME 두가지 책임을 동시에 지고 있습니다. + Save에는 트랜잭셔널 어노테이션이 필요할까요?
     protected TokenResponse getTokenResponseDto(OAuthUserInfo oauthUser, User userEntity) {
         if (userEntity == null) {
             User userRequest = User.builder()
