8ae06bf0b598874ffada4aa2f527a689495da91a
LEESEUNGYEOL <lsy12210@gmail.com>
2024-08-01 17:58:56 +0900

    fix: [#203] UserService.delete 한 트랜잭션으로 수정

--- src/main/java/com/ssh/dartserver/domain/user/application/UserDeleter.java
+++ src/main/java/com/ssh/dartserver/domain/user/application/UserDeleter.java
@@ -1,5 +1,9 @@
 package com.ssh.dartserver.domain.user.application;
 
+import com.ssh.dartserver.domain.friend.application.FriendDeleter;
+import com.ssh.dartserver.domain.survey.application.AnswerUpdater;
+import com.ssh.dartserver.domain.survey.application.CommentUpdater;
+import com.ssh.dartserver.domain.team.application.TeamDeleter;
 import com.ssh.dartserver.domain.user.domain.User;
 import com.ssh.dartserver.domain.user.infra.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,11 +13,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @RequiredArgsConstructor
 public class UserDeleter {
+    private final FriendDeleter friendDeleter;
+    private final TeamDeleter teamDeleter;
+    private final CommentUpdater commentUpdater;
+    private final AnswerUpdater answerUpdater;
 
     private final UserRepository userRepository;
 
     @Transactional
     public void delete(User user) {
+        friendDeleter.deleteAllFriendShip(user);
+
+        teamDeleter.deleteAllTeamAndRelatedData(user);
+
+        //TODO: 커뮤니티 기능 제거 예정
+        answerUpdater.updateAllUserToNullInAnswer(user);
+        commentUpdater.updateAllUserToNullInCommentAndRelatedData(user);
+
+        //TODO: Soft Delete 적용 예정
         userRepository.delete(user);
     }
 }
--- src/main/java/com/ssh/dartserver/domain/user/application/UserService.java
+++ src/main/java/com/ssh/dartserver/domain/user/application/UserService.java
@@ -1,9 +1,5 @@
 package com.ssh.dartserver.domain.user.application;
 
-import com.ssh.dartserver.domain.friend.application.FriendDeleter;
-import com.ssh.dartserver.domain.survey.application.AnswerUpdater;
-import com.ssh.dartserver.domain.survey.application.CommentUpdater;
-import com.ssh.dartserver.domain.team.application.TeamDeleter;
 import com.ssh.dartserver.domain.user.domain.User;
 import com.ssh.dartserver.domain.user.domain.personalinfo.PersonalInfo;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +11,6 @@ public class UserService {
 
     private final UserUpdater userUpdater;
     private final UserReader userReader;
-    private final FriendDeleter friendDeleter;
-    private final TeamDeleter teamDeleter;
-    private final CommentUpdater commentUpdater;
-    private final AnswerUpdater answerUpdater;
     private final UserDeleter userDeleter;
 
     public User signUp(User user, PersonalInfo personalInfo, Long universityId) {
@@ -34,15 +26,6 @@ public class UserService {
     }
 
     public void delete(User user) {
-        friendDeleter.deleteAllFriendShip(user);
-
-        teamDeleter.deleteAllTeamAndRelatedData(user);
-
-        //TODO: 커뮤니티 기능 제거 예정
-        answerUpdater.updateAllUserToNullInAnswer(user);
-        commentUpdater.updateAllUserToNullInCommentAndRelatedData(user);
-
-        //TODO: soft Delete로 적용 예정
         userDeleter.delete(user);
     }
 
