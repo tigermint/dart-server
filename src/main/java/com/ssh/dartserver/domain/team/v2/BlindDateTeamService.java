8d202e1936e42b2405127b35faddb0cb6a7dca12
Basak <chgy2131@naver.com>
2024-10-19 02:06:03 +0900

    feat: 팀 상세 조회 기능 추가
    - TODO: 추후 v1, v2 팀인지를 구분하여 분기해주는 처리가 필요하다.

--- src/main/java/com/ssh/dartserver/domain/team/v2/BlindDateTeamService.java
+++ src/main/java/com/ssh/dartserver/domain/team/v2/BlindDateTeamService.java
@@ -2,6 +2,8 @@ package com.ssh.dartserver.domain.team.v2;
 
 import com.ssh.dartserver.domain.image.application.ImageUploader;
 import com.ssh.dartserver.domain.image.domain.Image;
+import com.ssh.dartserver.domain.proposal.domain.Proposal;
+import com.ssh.dartserver.domain.proposal.infra.ProposalRepository;
 import com.ssh.dartserver.domain.team.domain.Region;
 import com.ssh.dartserver.domain.team.domain.Team;
 import com.ssh.dartserver.domain.team.domain.TeamImage;
@@ -10,6 +12,8 @@ import com.ssh.dartserver.domain.team.domain.vo.TeamDescription;
 import com.ssh.dartserver.domain.team.infra.RegionRepository;
 import com.ssh.dartserver.domain.team.infra.TeamRegionRepository;
 import com.ssh.dartserver.domain.team.infra.TeamRepository;
+import com.ssh.dartserver.domain.team.presentation.response.RegionResponse;
+import com.ssh.dartserver.domain.team.v2.dto.BlindDateTeamInfo;
 import com.ssh.dartserver.domain.team.v2.dto.CreateTeamRequest;
 import com.ssh.dartserver.domain.team.v2.dto.UpdateTeamRequest;
 import com.ssh.dartserver.domain.user.domain.User;
@@ -27,6 +31,7 @@ public class BlindDateTeamService {
     private final TeamImageRepository teamImageRepository;
     private final RegionRepository regionRepository;
     private final ImageUploader imageUploader;
+    private final ProposalRepository proposalRepository;
 
     // 팀 생성
     @Transactional
@@ -88,12 +93,56 @@ public class BlindDateTeamService {
 
     // 팀 목록 조회
     public void getTeamList(Pageable pageable) {
+
+        // TODO 조회수 처리 + 푸시 알림
+
+        // 성별 필터링은 해야함
+
         throw new UnsupportedOperationException();
     }
 
     // 팀 상세 조회
-    public void getTeamInfo(long teamId) {
-        throw new UnsupportedOperationException();
+    public BlindDateTeamInfo getTeamInfo(long teamId) {
+            // TODO 조회수 처리 + 푸시 알림
+
+        // TODO v1, v2 분기 처리
+//        if (team.getTeamUsersCombinationHash() != null) {
+//        }
+
+        Team team = teamRepository.findById(teamId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 팀 입니다. teamId: " + teamId));
+
+        List<RegionResponse> regions = team.getTeamRegions().stream()
+                .map(teamRegion -> new RegionResponse(teamRegion.getRegion().getId(), teamRegion.getRegion().getName()))
+                .toList();
+
+        List<String> images = team.getTeamImages().stream()
+                .map(teamImage -> teamImage.getImage().getData())
+                .toList();
+
+        // Proposal 확인하기 (개선 필요)
+        List<Proposal> proposals = proposalRepository.findAllByRequestingTeamOrRequestedTeam(team, team);
+        boolean isAlreadyProposal = proposals.stream()
+                .anyMatch(proposal -> team.getId() == proposal.getRequestedTeam().getId() || team.getId() == proposal.getRequestingTeam().getId());
+
+        // v2 처리
+        return BlindDateTeamInfo.builder()
+                .id(team.getId())
+                .leaderId(team.getLeader().getId())
+                .age(team.getLeader().getPersonalInfo().getBirthYear().getAge())
+                .isCertified(team.getLeader().getStudentVerificationInfo().isCertified())
+                .universityName(team.getLeader().getUniversity().getName())
+                .departmentName(team.getLeader().getUniversity().getDepartment())
+
+                .name(team.getName().getValue())
+                .description(team.getDescription().getDescription())
+                .isVisibleToSameUniversity(team.getIsVisibleToSameUniversity())
+
+                .regions(regions)
+                .imageUrls(images)
+                .isAlreadyProposalTeam(isAlreadyProposal)
+
+                .build();
     }
 
 }
--- src/test/java/com/ssh/dartserver/domain/team/v2/BlindDateTeamServiceTest.java
+++ src/test/java/com/ssh/dartserver/domain/team/v2/BlindDateTeamServiceTest.java
@@ -1,9 +1,14 @@
 package com.ssh.dartserver.domain.team.v2;
 
+import static com.ssh.dartserver.domain.university.UniversitySteps.대학생성요청_생성;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.ssh.dartserver.ApiTest;
+import com.ssh.dartserver.domain.image.domain.Image;
+import com.ssh.dartserver.domain.image.domain.ImageType;
+import com.ssh.dartserver.domain.image.infra.ImageRepository;
 import com.ssh.dartserver.domain.team.domain.Region;
 import com.ssh.dartserver.domain.team.domain.Team;
 import com.ssh.dartserver.domain.team.domain.TeamImage;
@@ -12,15 +17,22 @@ import com.ssh.dartserver.domain.team.domain.vo.TeamDescription;
 import com.ssh.dartserver.domain.team.infra.RegionRepository;
 import com.ssh.dartserver.domain.team.infra.TeamRegionRepository;
 import com.ssh.dartserver.domain.team.infra.TeamRepository;
+import com.ssh.dartserver.domain.team.v2.dto.BlindDateTeamInfo;
 import com.ssh.dartserver.domain.team.v2.dto.CreateTeamRequest;
+import com.ssh.dartserver.domain.university.domain.University;
+import com.ssh.dartserver.domain.university.infra.UniversityRepository;
 import com.ssh.dartserver.domain.user.domain.AuthInfo;
 import com.ssh.dartserver.domain.user.domain.User;
+import com.ssh.dartserver.domain.user.domain.personalinfo.BirthYear;
+import com.ssh.dartserver.domain.user.domain.personalinfo.Gender;
 import com.ssh.dartserver.domain.user.domain.personalinfo.PersonalInfo;
+import com.ssh.dartserver.domain.user.domain.studentverificationinfo.StudentVerificationInfo;
 import com.ssh.dartserver.domain.user.infra.UserRepository;
 import com.ssh.dartserver.global.common.Role;
 import com.ssh.dartserver.testing.IntegrationTest;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -48,12 +60,22 @@ class BlindDateTeamServiceTest extends ApiTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UniversityRepository universityRepository;
+
+    @Autowired
+    private ImageRepository imageRepository;
+
+
     @BeforeEach
     void setUp() {
         // DB에 지역 등록
         List.of("서울", "제주특별자치도", "NorthKorea123").forEach(
                 region -> regionRepository.save(Region.builder().name(region).build())
         );
+
+        List<University> universities = 대학생성요청_생성(3);
+        universityRepository.saveAll(universities);
     }
 
     @Nested
@@ -167,10 +189,119 @@ class BlindDateTeamServiceTest extends ApiTest {
 
     }
 
+    @Nested
+    @DisplayName("팀 상세 조회 테스트")
+    public class GetTeamInfo {
+
+        @Test
+        @DisplayName("등록한 팀의 id를 전달하면 정상적으로 조회한다.")
+        @Transactional
+        void getTeamInfo_Success_WhenTeamIsCreatedByUser() {
+            // Given: 사용자가 팀을 생성한 상황을 준비합니다.
+            User user = createUser();
+            Team team = Team.builder()
+                    .user(user)
+                    .name("멋진 TEAM")
+                    .description(new TeamDescription("팀 설명입니다."))
+                    .isVisibleToSameUniversity(true)
+                    .build();
+
+            List<Region> regions = regionRepository.findAllByIdIn(List.of(1L, 2L));
+            List<TeamRegion> teamRegions = regions.stream()
+                    .map(region -> TeamRegion.builder()
+                            .region(region)
+                            .team(team)
+                            .build())
+                    .toList();
+            teamRegions = teamRegionRepository.saveAll(teamRegions);
+
+            List<Image> images = List.of("http://example.com/image1.jpg", "http://example.com/image2.jpg").stream()
+                    .map(url -> new Image(null, ImageType.URL, url))
+                    .toList();
+            imageRepository.saveAll(images);
+            List<TeamImage> teamImages = images.stream()
+                    .map(image -> TeamImage.builder()
+                            .team(team)
+                            .image(image)
+                            .build())
+                    .toList();
+            teamImageRepository.saveAll(teamImages);
+
+            team.setTeamRegions(teamRegions);
+            team.setTeamImages(teamImages);
+            teamRepository.save(team);
+
+            // When: 내가 등록한 팀의 id로 팀 정보를 조회할 때
+            BlindDateTeamInfo teamInfo = blindDateTeamService.getTeamInfo(team.getId());
+
+            // Then: 팀 정보가 정상적으로 반환되어야 합니다. TODO v2 팀 전체 정보가 잘 들어오는지 확인!
+            assertThat(teamInfo).isNotNull();
+            assertAll(
+                    () -> assertThat(teamInfo.id()).isEqualTo(team.getId()),
+                    () -> assertThat(teamInfo.leaderId()).isEqualTo(user.getId()),
+                    () -> assertThat(teamInfo.name()).isEqualTo("멋진 TEAM"),
+                    () -> assertThat(teamInfo.description()).isEqualTo("팀 설명입니다."),
+                    () -> assertThat(teamInfo.isVisibleToSameUniversity()).isTrue(),
+                    () -> assertThat(teamInfo.age()).isEqualTo(25),  // 나이 값 설정
+                    () -> assertThat(teamInfo.isCertified()).isFalse(),
+                    () -> assertThat(teamInfo.universityName()).isEqualTo("Tech University 1000"),
+                    () -> assertThat(teamInfo.departmentName()).isEqualTo("컴퓨터공학과"),
+                    () -> assertThat(teamInfo.regions())
+                            .extracting("name")
+                            .containsExactlyInAnyOrder("서울", "제주특별자치도"),
+                    () -> assertThat(teamInfo.imageUrls())
+                            .containsExactly("http://example.com/image1.jpg", "http://example.com/image2.jpg"),
+                    () -> assertThat(teamInfo.isAlreadyProposalTeam()).isFalse()
+            );
+        }
+
+        @Test
+        @DisplayName("등록되지 않은 TeamId를 전달하면 예외가 발생한다.")
+        void getTeamInfo_ShouldThrowException_WhenTeamIdIsNotRegistered() {
+            // Given: 등록되지 않은 팀 id를 사용하여 조회 시도
+            long invalidTeamId = 999L;
+
+            // Expect: IllegalArgumentException 예외가 발생해야 합니다.
+            assertThrows(IllegalArgumentException.class, () -> {
+                blindDateTeamService.getTeamInfo(invalidTeamId);
+            });
+        }
+
+        @Test
+        @DisplayName("TeamId로 0 또는 음수가 주어지면 예외가 발생한다.")
+        void getTeamInfo_ShouldThrowException_WhenTeamIdIsZeroOrNegative() {
+            // Given: 0 또는 음수 팀 id를 사용하여 조회 시도
+            long zeroTeamId = 0L;
+            long negativeTeamId = -1L;
+
+            // Expect: IllegalArgumentException 예외가 발생해야 합니다.
+            assertThrows(IllegalArgumentException.class, () -> {
+                blindDateTeamService.getTeamInfo(zeroTeamId);
+            });
+
+            assertThrows(IllegalArgumentException.class, () -> {
+                blindDateTeamService.getTeamInfo(negativeTeamId);
+            });
+        }
+
+        @Disabled
+        @Test
+        @DisplayName("v1 팀의 id를 전달하면 정상적으로 조회한다.")
+        void getTeamInfo_Success_WhenTeamIdIsValid() {
+            throw new UnsupportedOperationException();  // TODO 기능 구현 필요 + 추가로 v1 클라이언트의 요청도 커버해야함 (이건 기존 서비스 테스트)
+        }
+
+    }
+
     private User createUser() {
         return userRepository.save(User.builder()
                 .authInfo(AuthInfo.of("testUser", "kakao", "12345678"))
-                .personalInfo(PersonalInfo.builder().build())
+                .personalInfo(PersonalInfo.builder()
+                        .birthYear(BirthYear.from(2000))
+                        .gender(Gender.MALE)
+                        .build())
+                .university(universityRepository.findById(1L).get())
+                .studentVerificationInfo(StudentVerificationInfo.newInstance())
                 .role(Role.USER)
                 .build());
     }
